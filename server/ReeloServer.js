9dbda64d12fb7afff7f142df4e4796555aa07a9a
Prachi Sahay <sahaykhushi350@gmail.com>
2023-11-19 00:47:32 +0530

    Changes to port

--- server/ReeloServer.js
+++ server/ReeloServer.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 3001;
 const {generateQuestionPaper}=require('./GenerateQuestionPaper.js')
 
 
